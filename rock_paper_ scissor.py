857eb70b2ff66e0d16f0810531cfe75d0350446f
akshat01-ar <akshatgautamg1@gmail.com>
2025-03-10 22:46:07 +0530

    rock_paper_ scissor.py

--- rock_paper_ scissor.py
+++ rock_paper_ scissor.py
@@ -7,24 +7,31 @@ bot_choice = random.choice(item)
 
 print("user move :",user_ip)
 print()
-print("bot move : ",bot_choice)
+#print("bot move : ",bot_choice)
 
 
 
 if(bot_choice == user_ip):
+    print("bot move : ",bot_choice)
     print(" match is draww... baby")
 
 elif(bot_choice == "Rock" and user_ip== "Paper"):
+    print("bot move : ",bot_choice)
     print("***** You Win Baby *****")
 elif(bot_choice == "Rock" and user_ip == "Scissor"):
+    print("bot move : ",bot_choice)
     print("Bot Wins, (you loose basically)")
 elif(bot_choice == "Paper" and user_ip == "Rock"):
+    print("bot move : ",bot_choice)
     print("Bot Wins, (you loose basically")
 elif(bot_choice == " Paper" and user_ip == " Scissor"):
+    print("bot move : ",bot_choice)
     print("***** You Win Baby *****")
 elif(bot_choice == "Scissor" and user_ip == "Rock"):
+    print("bot move : ",bot_choice)
     print("***** You Win Baby *****")
 elif(bot_choice == "Scissor" and user_ip == "Paper"):
+    print("bot move : ",bot_choice)
     print("Bot Wins, (you loose basically)")
 else:
     print(" Give input correctly Master")
